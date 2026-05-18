6d5c2bb5c9013d8bad329910432c65ed3015cc13
juandkk <juandiegocs2004@gmail.com>
2026-05-17 20:04:13 -0500

    proyecto segundo modulo

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application } from '@nativescript/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'Featured',
   templateUrl: './featured.component.html',
 })
 export class FeaturedComponent implements OnInit {
-  constructor() {
-    // Use the component constructor to inject providers.
-  }
+
+  movieName!: string
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    // Init your component properties here.
+
+    this.movieName =
+      this.route.snapshot.queryParams.name
+
   }
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
   }
+
 }
